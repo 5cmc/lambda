45a19ffd837d6eccd96a16900e5b4f09bca0c75e
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2022-02-23 19:09:05 -0800

    Deploy elytra from midair xxx

--- src/main/kotlin/com/lambda/client/module/modules/movement/ElytraFlight2b2t.kt
+++ src/main/kotlin/com/lambda/client/module/modules/movement/ElytraFlight2b2t.kt
@@ -7,35 +7,28 @@ import com.lambda.client.event.events.PacketEvent
 import com.lambda.client.event.events.PlayerMoveEvent
 import com.lambda.client.event.events.PlayerTravelEvent
 import com.lambda.client.manager.managers.PlayerPacketManager.sendPlayerPacket
-import com.lambda.client.manager.managers.TimerManager.resetTimer
-import com.lambda.client.mixin.extension.isInWeb
 import com.lambda.client.mixin.extension.tickLength
 import com.lambda.client.mixin.extension.timer
 import com.lambda.client.module.Category
 import com.lambda.client.module.Module
 import com.lambda.client.module.modules.player.ViewLock
-import com.lambda.client.util.BaritoneUtils
-import com.lambda.client.util.EntityUtils.isInOrAboveLiquid
-import com.lambda.client.util.MovementUtils
-import com.lambda.client.util.MovementUtils.applySpeedPotionEffects
-import com.lambda.client.util.MovementUtils.calcMoveYaw
 import com.lambda.client.util.MovementUtils.setSpeed
-import com.lambda.client.util.MovementUtils.speed
 import com.lambda.client.util.TickTimer
 import com.lambda.client.util.TimeUnit
 import com.lambda.client.util.math.Vec2f
 import com.lambda.client.util.threads.safeAsyncListener
 import com.lambda.client.util.threads.safeListener
 import com.lambda.client.util.world.getGroundPos
-import net.minecraft.client.settings.KeyBinding
 import net.minecraft.init.Items
+import net.minecraft.inventory.ClickType
+import net.minecraft.item.ItemElytra
+import net.minecraft.item.ItemStack
+import net.minecraft.network.play.client.CPacketConfirmTeleport
 import net.minecraft.network.play.client.CPacketEntityAction
 import net.minecraft.network.play.server.SPacketPlayerPosLook
 import net.minecraftforge.client.event.InputUpdateEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import java.time.Instant
-import kotlin.math.cos
-import kotlin.math.sin
 
 object ElytraFlight2b2t : Module(
     name = "ElytraFlight2b2t",
@@ -48,15 +41,11 @@ object ElytraFlight2b2t : Module(
     private val boostAcceleration by setting("Boost speed acceleration", 1.02, 1.00..2.0, 0.001)
     private val takeoffTimerSpeed by setting("Takeoff Timer Tick Length", 250.0f, 100.0f..1000.0f, 1.0f)
 
-    private val strafeAirSpeedBoost = 0.016f
     private val baseFlightSpeed: Double = 40.2
     private var currentState = State.PAUSED
     private var timer = TickTimer(TimeUnit.TICKS)
-    private var flightHeight: Double = 0.0
     private var currentFlightSpeed: Double = 40.2
     private var shouldStartBoosting: Boolean = false;
-    private val strafeTimer = TickTimer(TimeUnit.TICKS)
-    private var jumpTicks = 0
     private var elytraIsEquipped = false
     private var elytraDurability = 0
     private var wasInLiquid: Boolean = false
@@ -64,9 +53,11 @@ object ElytraFlight2b2t : Module(
     private var isStandingStill = false
     private var isStandingStillH: Boolean = false
     private var lastSPacketPlayerPosLook: Long = Instant.now().toEpochMilli()
+    private var unequipedElytra: Boolean = false
+    private var shouldHover: Boolean = false
 
     enum class State {
-        FLYING, WALKING, PAUSED
+        FLYING, PRETAKEOFF, PAUSED, HOVER
     }
 
     init {
@@ -84,6 +75,8 @@ object ElytraFlight2b2t : Module(
             mc.timer.tickLength = 50.0f
             wasInLiquid = false
             isFlying = false
+            shouldHover = false
+            unequipedElytra = false
         }
 
         safeListener<ConnectionEvent.Disconnect> {
@@ -97,20 +90,40 @@ object ElytraFlight2b2t : Module(
 
             when (currentState) {
                 State.PAUSED -> {
-                    flightHeight = mc.player.posY + 1.0
-                    currentState = State.WALKING
+                    currentState = State.PRETAKEOFF
                 }
-                State.WALKING -> {
-                    val currentY = mc.player.posY
-                    // need to add some leniency here for actual coord being a long decimal
-                    if (
-                        (currentY >= flightHeight - 0.05 && currentY <= flightHeight + 0.05 || currentY > flightHeight + 1)
-                            || mc.player.isElytraFlying) {
+                State.PRETAKEOFF -> {
+                    val expectedMotionY = -0.16976
+                    val height = 1.5
+                    val notCloseToGround = player.posY >= world.getGroundPos(player).y + height && !wasInLiquid && !mc.isSingleplayer
+
+                    if ((withinRange(mc.player.motionY, expectedMotionY, 0.05)) && !mc.player.isElytraFlying) {
                         currentState = State.FLYING
                         timer.reset()
+                    } else if (notCloseToGround && !unequipedElytra) {
+                        currentState = State.HOVER
+                    }
+                }
+                State.HOVER -> {
+                    val armorSlot = player.inventory.armorInventory[2]
+                    elytraIsEquipped = armorSlot.item == Items.ELYTRA
+                    if (elytraIsEquipped && !unequipedElytra) {
+                        mc.playerController.windowClick(0, 6, 0,
+                            ClickType.QUICK_MOVE, mc.player)
+                        unequipedElytra = true
+                        shouldHover = true
+                    } else if (!elytraIsEquipped && unequipedElytra) {
+                        var slot: Int = getSlotOfNextElytra()
+                        mc.playerController.windowClick(0, slot, 0, ClickType.QUICK_MOVE, mc.player)
+                        shouldHover = true
+                    } else if (elytraIsEquipped && unequipedElytra) {
+                        // ok, ready to fall
+                        shouldHover = false
+                        currentState = State.PRETAKEOFF
                     }
                 }
                 State.FLYING -> {
+                    unequipedElytra = false
                     if (shouldStartBoosting) {
                         if (timer.tick(ticksBetweenBoosts, true)) {
                             setFlightSpeed(currentFlightSpeed * boostAcceleration)
@@ -125,18 +138,23 @@ object ElytraFlight2b2t : Module(
         }
 
         safeAsyncListener<PacketEvent.Receive> {
-            if (currentState != State.FLYING || it.packet !is SPacketPlayerPosLook) return@safeAsyncListener
-            timer.reset()
-            resetFlightSpeed()
-            if (Instant.now().toEpochMilli() - lastSPacketPlayerPosLook < 500L) {
-                LambdaMod.LOG.info("Rubberband detected")
-                mc.player.capabilities.isFlying = false
+            if (it.packet !is SPacketPlayerPosLook) return@safeAsyncListener
+            if (currentState == State.FLYING) {
+                timer.reset()
+                resetFlightSpeed()
+                if (Instant.now().toEpochMilli() - lastSPacketPlayerPosLook < 200L) {
+                    LambdaMod.LOG.info("Rubberband detected")
+                    currentState = State.PRETAKEOFF
+                }
+                lastSPacketPlayerPosLook = Instant.now().toEpochMilli()
+            } else if (shouldHover) {
+                it.cancel()
+                connection.sendPacket(CPacketConfirmTeleport(it.packet.teleportId))
             }
-            lastSPacketPlayerPosLook = Instant.now().toEpochMilli()
+
         }
 
         safeListener<PlayerTravelEvent> {
-            if (shouldStrafe()) strafe()
             stateUpdate(it)
             if (currentState == State.FLYING) {
                 if (elytraIsEquipped && elytraDurability > 1) {
@@ -146,18 +164,17 @@ object ElytraFlight2b2t : Module(
                         mc.timer.tickLength = 50.0f
                         player.isSprinting = false
                     }
-                    spoofRotation()
                 }
             }
+            spoofRotation()
         }
 
         safeListener<PlayerMoveEvent> {
-            if (shouldStrafe()) setSpeed(java.lang.Double.max(player.speed, applySpeedPotionEffects(0.2873)))
-            else {
-                reset()
-            }
-            if (currentState == State.FLYING) {
+            if (currentState == State.FLYING && mc.player.isElytraFlying) {
                 setSpeed(currentFlightSpeed / 10.0)
+                player.motionY = 0.0
+            } else if (shouldHover) {
+                setSpeed(0.0)
                 player.motionY = 0.0
             }
         }
@@ -173,50 +190,38 @@ object ElytraFlight2b2t : Module(
         }
     }
 
+    private fun withinRange(num: Double, target: Double, range: Double): Boolean {
+        return (num >= target - range && num <= target + range)
+    }
+
     private fun resetFlightSpeed() {
         setFlightSpeed(baseFlightSpeed)
     }
 
+    private fun getSlotOfNextElytra(): Int {
+        (0..44).forEach { slot ->
+            val stack = mc.player.inventory.getStackInSlot(slot)
+            if (stack.item !is ItemElytra) return@forEach
+
+            if (stack.count > 1) return@forEach
+
+            if (!isItemBroken(stack)) {
+                return slot
+            }
+        }
+        return -1
+    }
+
+    private fun isItemBroken(itemStack: ItemStack): Boolean { // (100 * damage / max damage) >= (100 - 70)
+        return if (itemStack.maxDamage == 0) {
+            false
+        } else {
+            itemStack.maxDamage - itemStack.itemDamage <= 2
+        }
+    }
+
     private fun setFlightSpeed(speed: Double) {
         currentFlightSpeed = speed
-    }
-
-    private fun SafeClientEvent.shouldStrafe(): Boolean =
-        (!player.capabilities.isFlying
-            && !player.isElytraFlying
-            && !mc.gameSettings.keyBindSneak.isKeyDown
-            && !BaritoneUtils.isPathing
-            && MovementUtils.isInputting
-            && !(player.isInOrAboveLiquid || player.isInWeb))
-
-    private fun SafeClientEvent.strafe() {
-        player.jumpMovementFactor = strafeAirSpeedBoost
-        jump()
-
-        strafeTimer.reset()
-    }
-
-    private fun SafeClientEvent.jump() {
-        if (player.onGround && jumpTicks <= 0) {
-            if (player.isSprinting) {
-                val yaw = calcMoveYaw()
-                player.motionX -= sin(yaw) * 0.2
-                player.motionZ += cos(yaw) * 0.2
-            }
-
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindJump.keyCode, false)
-            player.motionY = 0.4
-            player.isAirBorne = true
-            jumpTicks = 5
-        }
-
-        jumpTicks--
-    }
-
-    private fun SafeClientEvent.reset() {
-        player.jumpMovementFactor = 0.02f
-        resetTimer()
-        jumpTicks = 0
     }
 
     private fun SafeClientEvent.stateUpdate(event: PlayerTravelEvent) {
@@ -247,7 +252,6 @@ object ElytraFlight2b2t : Module(
 
     /* The best takeoff method <3 */
     private fun SafeClientEvent.takeoff(event: PlayerTravelEvent) {
-        /* Pause Takeoff if server is lagging, player is in water/lava, or player is on ground */
         val timerSpeed = takeoffTimerSpeed
         val height = 0.1
         val closeToGround = player.posY <= world.getGroundPos(player).y + height && !wasInLiquid && !mc.isSingleplayer
@@ -271,13 +275,12 @@ object ElytraFlight2b2t : Module(
     private fun SafeClientEvent.spoofRotation() {
         if (player.isSpectator || !elytraIsEquipped || elytraDurability <= 1 || !isFlying) return
 
-        var cancelRotation = false
         var rotation = Vec2f(player)
 
         if (!isStandingStill) rotation = Vec2f(rotation.x, -2.02f)
 
         /* Cancels rotation packets if player is not moving and not clicking */
-        cancelRotation = isStandingStill && ((!mc.gameSettings.keyBindUseItem.isKeyDown && !mc.gameSettings.keyBindAttack.isKeyDown))
+        var cancelRotation = isStandingStill && ((!mc.gameSettings.keyBindUseItem.isKeyDown && !mc.gameSettings.keyBindAttack.isKeyDown))
 
         sendPlayerPacket {
             if (cancelRotation) {
