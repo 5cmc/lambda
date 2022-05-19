0ab896e98714c73c906fee60233cf82440478181
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2022-05-19 00:21:25 -0700

    Use baritone to path forward ElytraFLightHighway on rubberband instead of waiting

--- src/main/kotlin/com/lambda/client/module/modules/movement/ElytraFlightHighway.kt
+++ src/main/kotlin/com/lambda/client/module/modules/movement/ElytraFlightHighway.kt
@@ -1,15 +1,18 @@
 package com.lambda.client.module.modules.movement
 
+import baritone.api.pathing.goals.GoalXZ
 import com.lambda.client.LambdaMod
 import com.lambda.client.event.events.PacketEvent
 import com.lambda.client.module.Category
 import com.lambda.client.module.Module
 import com.lambda.client.module.modules.player.ViewLock
+import com.lambda.client.util.BaritoneUtils
 import com.lambda.client.util.TickTimer
 import com.lambda.client.util.TimeUnit
 import com.lambda.client.util.threads.safeAsyncListener
 import com.lambda.client.util.threads.safeListener
 import net.minecraft.network.play.server.SPacketPlayerPosLook
+import net.minecraft.util.math.BlockPos
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import java.time.Instant
 
@@ -19,42 +22,42 @@ object ElytraFlightHighway : Module(
     category = Category.MOVEMENT,
     modulePriority = 1000
 ) {
-    private val waitTicks by setting("Rubberband Wait Ticks", 80, 0..300, 1)
+    private val baritonePathForwardBlocks by setting("Rubberband Path Distance", 20, 1..50, 1)
 
-    private var currentState = State.PAUSED
+    private var currentState = State.WALKING
     private var timer = TickTimer(TimeUnit.TICKS)
     private var lastSPacketPlayerPosLook: Long = Instant.now().toEpochMilli()
     private var flyTickCount = 0
-
+    private var flyPlayerLastPos: BlockPos? = null
+    private var flyBlockedTickCount = 0
 
     enum class State {
-        FLYING, WALKING, PAUSED
+        FLYING, TAKEOFF, WALKING
     }
 
     init {
 
         onEnable {
-            currentState = State.WALKING
-            toggleAllOff()
+            currentState = State.TAKEOFF
             toggleAllOn()
         }
 
         onDisable {
-            currentState = State.PAUSED
+            currentState = State.WALKING
             toggleAllOff()
+            stopPathing()
         }
 
         safeListener<TickEvent.ClientTickEvent> {
             if (it.phase != TickEvent.Phase.END) return@safeListener
 
             when (currentState) {
-                State.PAUSED -> {
-                    if (timer.tick(waitTicks)) {
-                        currentState = State.WALKING
-                        toggleAllOn()
-                    }
-                }
                 State.WALKING -> {
+                    while (isPathing()) return@safeListener
+                    currentState = State.TAKEOFF
+                    toggleAllOn()
+                }
+                State.TAKEOFF -> {
                     if (mc.player.isElytraFlying) {
                         currentState = State.FLYING
                     }
@@ -63,29 +66,58 @@ object ElytraFlightHighway : Module(
                     if (!mc.player.isElytraFlying) {
                         if (flyTickCount++ > 30) {
                             toggleAllOff()
+                            pathForward()
                             timer.reset()
-                            currentState = State.PAUSED
+                            currentState = State.WALKING
                         }
                     } else {
                         flyTickCount = 0
                     }
+                    val playerCurrentPos = mc.player.position
+                    if (playerCurrentPos.equals(flyPlayerLastPos)) {
+                        if (flyBlockedTickCount++ > 20) {
+                            toggleAllOff()
+                            pathForward()
+                            timer.reset()
+                            currentState = State.WALKING
+                        }
+                    } else {
+                        flyBlockedTickCount = 0
+                    }
+                    flyPlayerLastPos = playerCurrentPos
                 }
             }
         }
 
         safeAsyncListener<PacketEvent.Receive> {
-            if ((currentState != State.FLYING && currentState != State.WALKING) || it.packet !is SPacketPlayerPosLook) return@safeAsyncListener
+            if ((currentState != State.FLYING && currentState != State.TAKEOFF) || it.packet !is SPacketPlayerPosLook) return@safeAsyncListener
             val now = Instant.now().toEpochMilli()
             if (now - lastSPacketPlayerPosLook < 1000L) {
                 LambdaMod.LOG.info("Rubberband detected")
-                currentState = State.PAUSED
                 toggleAllOff()
-                timer.reset()
+                pathForward()
+                currentState = State.WALKING
             }
             lastSPacketPlayerPosLook = now
         }
     }
 
+    private fun pathForward() {
+        val playerContext = BaritoneUtils.primary?.playerContext!!
+        BaritoneUtils.primary?.customGoalProcess?.setGoalAndPath(GoalXZ.fromDirection(
+            playerContext.playerFeetAsVec(),
+            playerContext.player().rotationYawHead,
+            baritonePathForwardBlocks.toDouble()
+        ))
+    }
+
+    private fun stopPathing() {
+        BaritoneUtils.cancelEverything()
+    }
+
+    private fun isPathing(): Boolean {
+        return BaritoneUtils.isActive
+    }
 
     private fun toggleAllOff() {
         ElytraFlight2b2t.disable()
