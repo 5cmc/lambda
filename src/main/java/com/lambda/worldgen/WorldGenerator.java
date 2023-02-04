5c70eefc860ea76351fc09315541790e3128ae97
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-02-03 23:05:18 -0800

    seed overlay

---
+++ src/main/java/com/lambda/worldgen/WorldGenerator.java
@@ -0,0 +1,379 @@
+package com.lambda.worldgen;
+
+import com.mojang.authlib.GameProfileRepository;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
+import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
+import com.mojang.authlib.yggdrasil.YggdrasilGameProfileRepository;
+import net.minecraft.client.ClientBrandRetriever;
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.ServerCommandManager;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.crash.ICrashReportDetail;
+import net.minecraft.profiler.Snooper;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerList;
+import net.minecraft.server.management.PlayerProfileCache;
+import net.minecraft.util.CryptManager;
+import net.minecraft.util.Util;
+import net.minecraft.world.*;
+import net.minecraft.world.storage.ISaveHandler;
+import net.minecraft.world.storage.WorldInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.util.UUID;
+
+public class WorldGenerator extends MinecraftServer {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+    private final WorldSettings worldSettings;
+    public boolean done = false;
+    private boolean isGamePaused;
+    private static final Minecraft mc = Minecraft.getMinecraft();
+
+    public static WorldGenerator create(WorldSettings settings) {
+        YggdrasilAuthenticationService as = new YggdrasilAuthenticationService(mc.getProxy(), UUID.randomUUID().toString());
+        YggdrasilGameProfileRepository gpr = new YggdrasilGameProfileRepository(as);
+        return new WorldGenerator(mc, settings, as, as.createMinecraftSessionService(), gpr, new PlayerProfileCache(gpr, new File("lambdaSeedOverlay/usercache.json")));
+    }
+
+    public static void deleteDir(File dir) {
+        try {
+            File[] files = dir.listFiles();
+
+            if (files != null) {
+                for(File file : files) {
+                    if (file.isDirectory()) {
+                        deleteDir(file);
+                    } else {
+                        file.delete();
+                    }
+                }
+            }
+
+            dir.delete();
+        } catch (Exception var6) {
+        }
+    }
+
+    public WorldGenerator(Minecraft clientIn, WorldSettings worldSettingsIn, YggdrasilAuthenticationService authServiceIn, MinecraftSessionService sessionServiceIn, GameProfileRepository profileRepoIn, PlayerProfileCache profileCacheIn)
+    {
+        super(new File("lambdaSeedOverlay/saves"), clientIn.getProxy(), clientIn.getDataFixer(), authServiceIn, sessionServiceIn, profileRepoIn, profileCacheIn);
+
+        deleteDir(new File("lambdaSeedOverlay/saves/main"));
+        new File("lambdaSeedOverlay/saves/main").mkdirs();
+
+        this.setServerOwner("Lambda");
+        this.setFolderName("main");
+        this.setWorldName("main");
+        this.setDemo(clientIn.isDemo());
+        this.canCreateBonusChest(worldSettingsIn.isBonusChestEnabled());
+        this.setBuildLimit(256);
+        this.setPlayerList(new PlayerList(this) { });
+        this.worldSettings = this.isDemo() ? WorldServerDemo.DEMO_WORLD_SETTINGS : worldSettingsIn;
+    }
+
+    public ServerCommandManager createCommandManager()
+    {
+        return new ServerCommandManager(this);
+    }
+
+    public void loadAllWorlds(String saveName, String worldNameIn, long seed, WorldType type, String generatorOptions)
+    {
+        this.convertMapIfNeeded(saveName);
+        ISaveHandler isavehandler = this.getActiveAnvilConverter().getSaveLoader(saveName, true);
+        this.setResourcePackFromWorld(this.getFolderName(), isavehandler);
+        WorldInfo worldinfo = isavehandler.loadWorldInfo();
+
+        if (worldinfo == null)
+        {
+            worldinfo = new WorldInfo(this.worldSettings, worldNameIn);
+        }
+        else
+        {
+            worldinfo.setWorldName(worldNameIn);
+        }
+
+        if (false) { //Forge: Dead Code, implement below.
+            for (int i = 0; i < this.worlds.length; ++i)
+            {
+                int j = 0;
+
+                if (i == 1)
+                {
+                    j = -1;
+                }
+
+                if (i == 2)
+                {
+                    j = 1;
+                }
+
+                if (i == 0)
+                {
+                    if (this.isDemo())
+                    {
+                        this.worlds[i] = (WorldServer)(new WorldServerDemo(this, isavehandler, worldinfo, j, this.profiler)).init();
+                    }
+                    else
+                    {
+                        this.worlds[i] = (WorldServer)(new WorldServer(this, isavehandler, worldinfo, j, this.profiler)).init();
+                    }
+
+                    this.worlds[i].initialize(this.worldSettings);
+                }
+                else
+                {
+                    this.worlds[i] = (WorldServer)(new WorldServerMulti(this, isavehandler, j, this.worlds[0], this.profiler)).init();
+                }
+
+                this.worlds[i].addEventListener(new ServerWorldEventHandler(this, this.worlds[i]));
+            }
+        }// Forge: End Dead Code
+
+        WorldServer overWorld = (isDemo() ? (WorldServer)(new WorldServerDemo(this,isavehandler, worldinfo, 0, this.profiler)).init() :
+            (WorldServer)(new WorldServer(this, isavehandler, worldinfo, 0, this.profiler)).init());
+        overWorld.initialize(this.worldSettings);
+        for (int dim : net.minecraftforge.common.DimensionManager.getStaticDimensionIDs())
+        {
+            WorldServer world = (dim == 0 ? overWorld : (WorldServer)(new WorldServerMulti(this, isavehandler, dim, overWorld, this.profiler)).init());
+            world.addEventListener(new ServerWorldEventHandler(this, world));
+            if (!this.isSinglePlayer())
+            {
+                world.getWorldInfo().setGameType(getGameType());
+            }
+            net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.event.world.WorldEvent.Load(world));
+        }
+
+        this.getPlayerList().setPlayerManager(new WorldServer[]{ overWorld });
+
+        this.initialWorldChunkLoad();
+
+        done = true;
+    }
+
+    public boolean init() {
+        LOGGER.info("Starting integrated minecraft server version 1.12.2");
+        this.setOnlineMode(true);
+        this.setCanSpawnAnimals(true);
+        this.setCanSpawnNPCs(true);
+        this.setAllowPvp(true);
+        this.setAllowFlight(true);
+        LOGGER.info("Generating keypair");
+        this.setKeyPair(CryptManager.generateKeyPair());
+        if (!net.minecraftforge.fml.common.FMLCommonHandler.instance().handleServerAboutToStart(this)) return false;
+        this.loadAllWorlds(this.getFolderName(), this.getWorldName(), this.worldSettings.getSeed(), this.worldSettings.getTerrainType(), this.worldSettings.getGeneratorOptions());
+        this.setMOTD(this.getServerOwner() + " - " + this.worlds[0].getWorldInfo().getWorldName());
+        return net.minecraftforge.fml.common.FMLCommonHandler.instance().handleServerStarting(this);
+    }
+
+    public void tick()
+    {
+        boolean flag = this.isGamePaused;
+        this.isGamePaused = Minecraft.getMinecraft().getConnection() != null && Minecraft.getMinecraft().isGamePaused();
+
+        if (!flag && this.isGamePaused)
+        {
+            LOGGER.info("Saving and pausing game...");
+            this.getPlayerList().saveAllPlayerData();
+            this.saveAllWorlds(false);
+        }
+
+        if (this.isGamePaused)
+        {
+            synchronized (this.futureTaskQueue)
+            {
+                while (!this.futureTaskQueue.isEmpty())
+                {
+                    Util.runTask(this.futureTaskQueue.poll(), LOGGER);
+                }
+            }
+        }
+        else {
+            super.tick();
+
+            if (mc.gameSettings.renderDistanceChunks != this.getPlayerList().getViewDistance()) {
+                LOGGER.info("Changing view distance to {}, from {}", mc.gameSettings.renderDistanceChunks, this.getPlayerList().getViewDistance());
+                this.getPlayerList().setViewDistance(mc.gameSettings.renderDistanceChunks);
+            }
+
+            WorldInfo worldinfo1 = this.worlds[0].getWorldInfo();
+            WorldInfo worldinfo = mc.world.getWorldInfo();
+
+            if (!worldinfo1.isDifficultyLocked() && worldinfo.getDifficulty() != worldinfo1.getDifficulty()) {
+                LOGGER.info("Changing difficulty to {}, from {}", worldinfo.getDifficulty(), worldinfo1.getDifficulty());
+                this.setDifficultyForAllWorlds(worldinfo.getDifficulty());
+            } else if (worldinfo.isDifficultyLocked() && !worldinfo1.isDifficultyLocked()) {
+                LOGGER.info("Locking difficulty to {}", (Object) worldinfo.getDifficulty());
+
+                for (WorldServer worldserver : this.worlds) {
+                    if (worldserver != null) {
+                        worldserver.getWorldInfo().setDifficultyLocked(true);
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean canStructuresSpawn()
+    {
+        return false;
+    }
+
+    public GameType getGameType()
+    {
+        return this.worldSettings.getGameType();
+    }
+
+    public EnumDifficulty getDifficulty()
+    {
+        return mc.world.getWorldInfo().getDifficulty();
+    }
+
+    public boolean isHardcore()
+    {
+        return this.worldSettings.getHardcoreEnabled();
+    }
+
+    public boolean shouldBroadcastRconToOps()
+    {
+        return true;
+    }
+
+    public boolean shouldBroadcastConsoleToOps()
+    {
+        return true;
+    }
+
+    public void saveAllWorlds(boolean isSilent)
+    {
+        super.saveAllWorlds(isSilent);
+    }
+
+    public File getDataDirectory()
+    {
+        return new File("lambdaSeedOverlaySaves");
+    }
+
+    public boolean isDedicatedServer()
+    {
+        return false;
+    }
+
+    public boolean shouldUseNativeTransport()
+    {
+        return false;
+    }
+
+    public void finalTick(CrashReport report)
+    {
+        //this.mc.crashed(report);
+    }
+
+    public CrashReport addServerInfoToCrashReport(CrashReport report)
+    {
+        report = super.addServerInfoToCrashReport(report);
+        report.getCategory().addDetail("Type", new ICrashReportDetail<String>()
+        {
+            public String call() throws Exception
+            {
+                return "Integrated Server (map_client.txt)";
+            }
+        });
+        report.getCategory().addDetail("Is Modded", new ICrashReportDetail<String>()
+        {
+            public String call() throws Exception
+            {
+                String s = ClientBrandRetriever.getClientModName();
+
+                if (!s.equals("vanilla"))
+                {
+                    return "Definitely; Client brand changed to '" + s + "'";
+                }
+                else
+                {
+                    s = WorldGenerator.this.getServerModName();
+
+                    if (!"vanilla".equals(s))
+                    {
+                        return "Definitely; Server brand changed to '" + s + "'";
+                    }
+                    else
+                    {
+                        return Minecraft.class.getSigners() == null ? "Very likely; Jar signature invalidated" : "Probably not. Jar signature remains and both client + server brands are untouched.";
+                    }
+                }
+            }
+        });
+        return report;
+    }
+
+    public void setDifficultyForAllWorlds(EnumDifficulty difficulty)
+    {
+        super.setDifficultyForAllWorlds(difficulty);
+    }
+
+    @Override
+    public void addServerStatsToSnooper(Snooper playerSnooper)
+    {
+        //super.addServerStatsToSnooper(playerSnooper);
+    }
+
+    public boolean isSnooperEnabled()
+    {
+        return Minecraft.getMinecraft().isSnooperEnabled();
+    }
+
+    @Override
+    public String shareToLAN(GameType type, boolean allowCheats)
+    {
+        return "";
+    }
+
+    public void stopServer()
+    {
+        initiateShutdown();
+        super.stopServer();
+    }
+
+    public void initiateShutdown()
+    {
+        // No need to check that, you cant join it anyway!
+        /*
+        if (isServerRunning())
+            Futures.getUnchecked(this.addScheduledTask(new Runnable()
+            {
+                public void run()
+                {
+                    for (EntityPlayerMP entityplayermp : Lists.newArrayList(WorldGenerator.this.getPlayerList().getPlayers()))
+                    {
+                        if (!entityplayermp.getUniqueID().equals(WorldGenerator.this.mc.player.getUniqueID()))
+                        {
+                            WorldGenerator.this.getPlayerList().playerLoggedOut(entityplayermp);
+                        }
+                    }
+                }
+            }));
+
+         */
+        super.initiateShutdown();
+
+    }
+
+    public void setGameType(GameType gameMode)
+    {
+        super.setGameType(gameMode);
+        this.getPlayerList().setGameType(gameMode);
+    }
+
+    public boolean isCommandBlockEnabled()
+    {
+        return true;
+    }
+
+    public int getOpPermissionLevel()
+    {
+        return 4;
+    }
+}
---
+++ src/main/kotlin/com/lambda/client/module/modules/render/SeedOverlay.kt
@@ -0,0 +1,237 @@
+package com.lambda.client.module.modules.render
+
+import com.lambda.client.LambdaMod
+import com.lambda.client.event.SafeClientEvent
+import com.lambda.client.event.events.RenderWorldEvent
+import com.lambda.client.module.Category
+import com.lambda.client.module.Module
+import com.lambda.client.util.color.ColorHolder
+import com.lambda.client.util.graphics.ESPRenderer
+import com.lambda.client.util.text.MessageSendHelper
+import com.lambda.client.util.threads.defaultScope
+import com.lambda.client.util.threads.safeListener
+import com.lambda.worldgen.WorldGenerator
+import kotlinx.coroutines.launch
+import net.minecraft.block.BlockFalling
+import net.minecraft.block.material.Material
+import net.minecraft.block.state.IBlockState
+import net.minecraft.init.Blocks
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+import net.minecraft.world.WorldSettings
+import net.minecraftforge.fml.common.gameevent.TickEvent
+import java.time.Duration
+import java.time.Instant
+
+object SeedOverlay: Module(
+    name = "SeedOverlay",
+    category = Category.RENDER,
+    description = "Render an overlay of blocks changed based on the current seed compared to the vanilla world"
+) {
+    /**
+     * todo:
+     *  paper generated world comparison (is this even feasible?)
+     */
+
+    private val overworldSeed by setting("Overworld Seed", "-4172144997902289642")
+    private val netherSeed by setting("Nether Seed", "146008555100680")
+    private val endSeed by setting("End Seed", "146008555100680")
+    private val renderNewBlocks by setting("Render New Blocks", true)
+    private val renderNewBlocksColor by setting("New Blocks Color", ColorHolder(255, 0, 0))
+    private val renderMissingBlocks by setting("Render Missing Blocks", true)
+    private val renderMissingBlocksColor by setting("Missing Blocks Color", ColorHolder(0, 255, 0))
+    private val renderDifferentBlocks by setting("Render Different Blocks", true)
+    private val renderDifferentBlocksColor by setting("Different Blocks Color", ColorHolder(0, 0, 255))
+    private val renderOutline by setting("Render Outline", true)
+    private val renderOutlineThickness by setting("Render Outline Thickness", 1.0, 0.1..3.0, 0.1)
+    private val renderFilled by setting("Render Filled", true)
+    private val renderFilledAlpha by setting("Render Fill Alpha", 100, 0..255, 1)
+    private val yMin by setting("Y Minimum", 30, 0..255, 1)
+    private val yMax by setting("Y Maximum", 90, 0..255, 1)
+    private val range by setting("Render Distance Range", 8, 1..16, 1)
+    private val maxRenderedDifferences by setting("Max Rendered", 5000, 500..50000, 100)
+
+    private var worldGenerator: WorldGenerator? = null
+    private var generatedWorld: World? = null
+    private var isUpdating = false
+    private var isInitializing = false
+    private val espRenderer = ESPRenderer()
+
+    private val differences = HashMap<BlockPos, Int>()
+
+    private val ignoreBlocks = listOf(Blocks.GLOWSTONE, Blocks.LOG, Blocks.LEAVES, Blocks.LOG2, Blocks.LEAVES2,
+        Blocks.COAL_ORE, Blocks.IRON_ORE, Blocks.GOLD_ORE, Blocks.LAPIS_ORE, Blocks.EMERALD_ORE, Blocks.DIAMOND_ORE,
+        Blocks.TALLGRASS, Blocks.DOUBLE_PLANT, Blocks.VINE, Blocks.YELLOW_FLOWER, Blocks.RED_FLOWER, Blocks.BROWN_MUSHROOM,
+        Blocks.RED_MUSHROOM, Blocks.BROWN_MUSHROOM_BLOCK, Blocks.RED_MUSHROOM_BLOCK, Blocks.FIRE, Blocks.DEADBUSH)
+
+    init {
+        onEnable {
+
+        }
+
+        onDisable {
+            try {
+                worldGenerator?.stopServer()
+            } catch (ex: Exception) {
+
+            }
+            worldGenerator = null
+            generatedWorld = null
+            isUpdating = false
+            isInitializing = false
+            clearToRender()
+        }
+
+        safeListener<TickEvent.ClientTickEvent> {
+            if (it.phase != TickEvent.Phase.START) return@safeListener
+            if (isUpdating || isInitializing) return@safeListener
+            if (mc.isSingleplayer) {
+                MessageSendHelper.sendChatMessage("Single Player Not Supported")
+                disable()
+                return@safeListener
+            }
+            if (worldGenerator == null || generatedWorld == null) {
+                isInitializing = true
+                defaultScope.launch {
+                    worldGenerator?.let { generator ->
+                        try {
+                            generator.stopServer()
+                        } catch (e: Exception) {
+
+                        }
+                    }
+                    try {
+                        worldGenerator = createFreshWorldCopy(Companion.mc.world, getSeedForCurrentWorld()!!)
+                        generatedWorld = worldGenerator!!.getWorld(Companion.mc.world.provider.dimension)
+                    } catch (ex: Exception) {
+                        worldGenerator = null
+                        generatedWorld = null
+                        disable()
+                        MessageSendHelper.sendChatMessage("Error starting SeedOverlay ${ex.message}")
+                    }
+                    isInitializing = false
+                }
+            }
+
+            generatedWorld?.let { genWorld ->
+                if (genWorld.provider.dimension != world.provider.dimension) {
+                    worldGenerator = null
+                    generatedWorld = null
+                    clearToRender()
+                    return@safeListener
+                }
+            }
+
+
+            isUpdating = true
+            defaultScope.launch {
+                try {
+                    update()
+                } catch (ex: Exception) {
+                    LambdaMod.LOG.warn("Error updating SeedOverlay", ex)
+                }
+                isUpdating = false
+            }
+        }
+
+        safeListener<RenderWorldEvent> {
+            if (renderFilled) {
+                espRenderer.aFilled = renderFilledAlpha
+            } else {
+                espRenderer.aFilled = 0
+            }
+            if (renderOutline) {
+                espRenderer.aOutline = 255
+                espRenderer.thickness = renderOutlineThickness.toFloat()
+            } else {
+                espRenderer.aOutline = 0
+            }
+            synchronized(differences) {
+                differences.entries.take(maxRenderedDifferences).forEach {
+                    when (it.value) {
+                        1 -> if (renderNewBlocks) espRenderer.add(it.key, renderNewBlocksColor)
+                        0 -> if (renderMissingBlocks) espRenderer.add(it.key, renderMissingBlocksColor)
+                        -1 -> if (renderDifferentBlocks) espRenderer.add(it.key, renderDifferentBlocksColor)
+                    }
+                }
+            }
+            espRenderer.render(true)
+        }
+    }
+
+    private fun SafeClientEvent.getSeedForCurrentWorld(): Long? {
+        return when(mc.world.provider.dimension) {
+            0 -> overworldSeed.toLongOrNull()
+            -1 -> netherSeed.toLongOrNull()
+            1 -> endSeed.toLongOrNull()
+            else -> null
+        }
+    }
+
+    private fun clearToRender() {
+        synchronized(differences) {
+            differences.clear()
+        }
+    }
+
+    private fun SafeClientEvent.update() {
+        val newRender: MutableMap<BlockPos, Int> = HashMap()
+
+        if (generatedWorld != null && worldGenerator != null) {
+            generatedWorld = worldGenerator!!.getWorld(world.provider.dimension)
+            for (z in -range * 16 until range * 16) {
+                for (x in -range * 16 until range * 16) {
+                    val theX = (player.posX + x).toInt()
+                    val theZ = (player.posZ + z).toInt()
+                    if (yMin >= yMax) return
+                    for (y in yMin..yMax) {
+                        val bp = BlockPos(theX, y, theZ)
+                        if (world.isBlockLoaded(bp, false) && generatedWorld!!.getChunk(bp).isTerrainPopulated) {
+                            val a: IBlockState = world.getBlockState(bp)
+                            val b = generatedWorld!!.getBlockState(bp)
+                            if (a.material != b.material) {
+                                if (!a.material.isLiquid && !b.material.isLiquid &&
+                                    !BlockFalling::class.java.isAssignableFrom(a.block.javaClass) && !BlockFalling::class.java.isAssignableFrom(b.block.javaClass) &&
+                                    !ignoreBlocks.contains(a.block) && !ignoreBlocks.contains(b.block)) {
+                                    if (a.material === Material.AIR) {
+                                        newRender[bp] = -1 // block in player world not in generated
+                                    } else if (b.material === Material.AIR) {
+                                        newRender[bp] = 1 // block in generated world but not in player world
+                                    } else {
+                                        newRender[bp] = 0 // different block in both
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        synchronized(differences) {
+            differences.clear()
+            differences.putAll(newRender)
+        }
+    }
+
+    private fun createFreshWorldCopy(worldIn: World, seed: Long): WorldGenerator {
+        val i = worldIn.worldInfo
+
+        val nbt = i.cloneNBTCompound(null)
+        nbt.setLong("RandomSeed", seed)
+        val settings = WorldSettings(seed, worldIn.worldInfo.gameType, true, false, worldIn.worldType)
+        settings.generatorOptions = worldIn.worldInfo.generatorOptions
+
+        val w: WorldGenerator = WorldGenerator.create(settings)
+        w.startServerThread()
+        val before = Instant.now()
+        while (!w.done) {
+            MessageSendHelper.sendChatMessage("${w.percentDone}% Generated")
+            Thread.sleep(1000)
+            if (Instant.now().minus(Duration.ofSeconds(30)).isAfter(before)) {
+                throw RuntimeException("Timed out creating world")
+            }
+        }
+        MessageSendHelper.sendChatMessage("Done generating world!")
+        return w
+    }
+}
