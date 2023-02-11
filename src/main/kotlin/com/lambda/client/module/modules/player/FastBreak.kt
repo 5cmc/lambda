ad42b3f066ed317eb5da5b3bff09dc48b361a2f0
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-02-11 00:36:49 -0800

    FastBreak: esp render the block being broken

--- src/main/kotlin/com/lambda/client/module/modules/player/FastBreak.kt
+++ src/main/kotlin/com/lambda/client/module/modules/player/FastBreak.kt
@@ -2,12 +2,19 @@ package com.lambda.client.module.modules.player
 
 import com.lambda.client.event.SafeClientEvent
 import com.lambda.client.event.events.PacketEvent
+import com.lambda.client.event.events.RenderWorldEvent
+import com.lambda.client.manager.managers.PlayerPacketManager.sendPlayerPacket
 import com.lambda.client.mixin.extension.blockHitDelay
 import com.lambda.client.module.Category
 import com.lambda.client.module.Module
 import com.lambda.client.util.TickTimer
 import com.lambda.client.util.TimeUnit
+import com.lambda.client.util.color.ColorHolder
+import com.lambda.client.util.graphics.ESPRenderer
+import com.lambda.client.util.math.RotationUtils.getRotationTo
+import com.lambda.client.util.math.VectorUtils.distanceTo
 import com.lambda.client.util.threads.safeListener
+import com.lambda.client.util.world.getHitVecOffset
 import net.minecraft.network.play.client.CPacketPlayerDigging
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
@@ -21,12 +28,14 @@ object FastBreak : Module(
 ) {
     private val breakDelay by setting("Break Delay", 0, 0..5, 1)
     private val packetMine by setting("Packet Mine", true)
-    private val sneakTrigger by setting("Sneak Trigger", true, { packetMine })
+    private val sneakTrigger by setting("Sneak Trigger", false, { packetMine })
     private val morePackets by setting("More Packets", false, { packetMine })
     private val spamDelay by setting("Spam Delay", 4, 1..10, 1, { packetMine })
+    // todo: tool swap
 
     private val spamTimer = TickTimer(TimeUnit.TICKS)
     private var miningInfo: Triple<Long, BlockPos, EnumFacing>? = null
+    private val espRenderer = ESPRenderer()
 
     init {
         onDisable {
@@ -38,6 +47,12 @@ object FastBreak : Module(
 
             if (breakDelay != 5 && playerController.blockHitDelay == 5) {
                 playerController.blockHitDelay = breakDelay
+            }
+
+            miningInfo?.let { mi ->
+                if (player.distanceTo(mi.second) > 5f) {
+                    miningInfo = null
+                }
             }
 
             if (packetMine) {
@@ -62,10 +77,22 @@ object FastBreak : Module(
                 it.cancel()
             }
         }
+
+        safeListener<RenderWorldEvent> {
+            miningInfo?.let {
+                espRenderer.aOutline = 255
+                espRenderer.aFilled = 150
+                espRenderer.add(it.second, ColorHolder(255, 0, 0, 100))
+                espRenderer.render(true)
+            }
+        }
     }
 
     private fun SafeClientEvent.doPacketMine(triple: Triple<Long, BlockPos, EnumFacing>?) {
         if (triple == null) return
+        sendPlayerPacket {
+            rotate(getRotationTo(getHitVecOffset(triple.third)))
+        }
 
         if (spamTimer.tick(spamDelay.toLong())) {
             val (startTime, pos, facing) = triple
@@ -74,6 +101,7 @@ object FastBreak : Module(
                 miningInfo = null
             } else {
                 if (morePackets) connection.sendPacket(CPacketPlayerDigging(CPacketPlayerDigging.Action.START_DESTROY_BLOCK, pos, facing))
+                // todo: calculate when we should send destroy block based on equipped pickaxe and block type
                 connection.sendPacket(CPacketPlayerDigging(CPacketPlayerDigging.Action.STOP_DESTROY_BLOCK, pos, facing))
             }
         }
