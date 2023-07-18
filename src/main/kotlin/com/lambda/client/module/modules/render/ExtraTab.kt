af6f918bee5f8c6157c2286f5953a64ff1f5bcfe
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-17 21:31:18 -0700

    cleanup and simplify extratab onlinetime

--- src/main/kotlin/com/lambda/client/module/modules/render/ExtraTab.kt
+++ src/main/kotlin/com/lambda/client/module/modules/render/ExtraTab.kt
@@ -3,7 +3,9 @@ package com.lambda.client.module.modules.render
 import com.google.gson.JsonParser
 import com.lambda.client.LambdaMod
 import com.lambda.client.commons.utils.ConnectionUtils
+import com.lambda.client.event.events.ConnectionEvent
 import com.lambda.client.event.events.PacketEvent
+import com.lambda.client.event.listener.listener
 import com.lambda.client.manager.managers.FriendManager
 import com.lambda.client.module.Category
 import com.lambda.client.module.Module
@@ -24,6 +26,7 @@ import net.minecraft.scoreboard.ScorePlayerTeam
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import java.time.Instant
 import java.time.ZonedDateTime
+import java.util.concurrent.ConcurrentHashMap
 
 object ExtraTab : Module(
     name = "ExtraTab",
@@ -31,7 +34,7 @@ object ExtraTab : Module(
     category = Category.RENDER
 ) {
     private val tabSize by setting("Max Players", 265, 80..400, 5)
-    val highlightFriends by setting("Highlight Friends", true)
+    private val highlightFriends by setting("Highlight Friends", true)
     private val color by setting("Color", EnumTextColor.GREEN, { highlightFriends })
     val ping by setting("Ping", Mode.SHOW)
     val onlineTime by setting("2b2t Online Times", true)
@@ -46,42 +49,50 @@ object ExtraTab : Module(
     private const val apiUrl = "https://api.2b2t.vc/tablist"
     private val parser = JsonParser()
     private val dataUpdateTimer = TickTimer(TimeUnit.SECONDS)
-    private val durationDataUpdateTimer = TickTimer(TimeUnit.SECONDS)
     private var hasInitialized = false
 
-    var tablistData = HashMap<String, HashMap<String, Any>>()
+    // playername -> joinedAt (epoch seconds)
+    private var tablistData = ConcurrentHashMap<String, Long>()
 
     init {
         onDisable {
             tablistData.clear()
             hasInitialized = false
         }
+
         safeListener<TickEvent.ClientTickEvent> {
-            if (onlineTime) {
-                if (dataUpdateTimer.tick(300L) // API caches tablist data for 5 minutes
-                    || !hasInitialized) {
-                    hasInitialized = true
-                    updateTabDataJoins()
-                } else if (durationDataUpdateTimer.tick(1L))
-                    updateTabDataDurations()
+            if (it.phase != TickEvent.Phase.END || !onlineTime) return@safeListener
+            if (dataUpdateTimer.tick(300L) // API caches tablist data for 5 minutes
+                || !hasInitialized) {
+                hasInitialized = true
+                updateTabDataJoins()
             }
         }
+
+        listener<ConnectionEvent.Disconnect> {
+            tablistData.clear()
+            hasInitialized = false
+        }
+
         safeListener<PacketEvent.Receive> {
-            if (onlineTime) {
-                if (it.packet !is SPacketPlayerListItem) return@safeListener
-                val now = Instant.now().epochSecond
-                if (it.packet.action == SPacketPlayerListItem.Action.ADD_PLAYER) {
-                    val name = it.packet.entries[0].profile.name
-                    if (name == null) return@safeListener
-                    tablistData[name] = HashMap()
-                    tablistData[name]?.set("joinedAt", now)
-                    tablistData[name]?.set("duration", 0)
-                    tablistData[name]?.set("expend", 0.0F)
-                } else if (it.packet.action == SPacketPlayerListItem.Action.REMOVE_PLAYER) {
-                    val name = it.packet.entries[0].profile.name
-                    if (name == null) return@safeListener
-                    tablistData.remove(name)
+            if (it.packet !is SPacketPlayerListItem || !onlineTime) return@safeListener
+            val now = Instant.now().epochSecond
+            when(it.packet.action) {
+                SPacketPlayerListItem.Action.ADD_PLAYER -> {
+                    it.packet.entries.forEach { data ->
+                        data.profile.name?.let { name ->
+                            tablistData[name] = now
+                        }
+                    }
                 }
+                SPacketPlayerListItem.Action.REMOVE_PLAYER -> {
+                    it.packet.entries.forEach { data ->
+                        data.profile.name?.let { name ->
+                            tablistData.remove(name)
+                        }
+                    }
+                }
+                else -> {}
             }
         }
     }
@@ -91,29 +102,22 @@ object ExtraTab : Module(
             runCatching {
                 ConnectionUtils.requestRawJsonFrom(apiUrl) {
                     LambdaMod.LOG.error("Failed querying queue data", it)
-                }?.let {
-                    tablistData.clear()
-                    val json = parser.parse(it).asJsonArray
-
-                    json.forEach {e -> tablistData[e.asJsonObject.get("playerName").asString] = HashMap()
-                        tablistData[e.asJsonObject.get("playerName").asString]?.set("joinedAt", ZonedDateTime.parse(e.asJsonObject.get("joinTime").asString).toEpochSecond())
+                }?.let { data ->
+                    val json = parser.parse(data).asJsonArray
+                    json.forEach { e ->
+                        val jsonObject = e.asJsonObject
+                        val playerName = jsonObject.get("playerName")?.asString
+                        val joinTime = jsonObject.get("joinTime")?.asString
+                        playerName?.let { name -> joinTime?.let{ time ->
+                            mc.connection?.playerInfoMap?.find { it.gameProfile.name == name }?.let {
+                                tablistData[name] = ZonedDateTime.parse(time).toEpochSecond()
+                            }
+                        }}
                     }
-                    updateTabDataDurations()
-                    return@runCatching
                 }
             }
         }
     }
-    private fun updateTabDataDurations() {
-        val now = Instant.now().epochSecond
-        for (key in tablistData.keys) {
-            val joinedAt = (tablistData[key]?.get("joinedAt") as Long?)!!
-            val duration = (now - joinedAt)
-            tablistData[key]?.set("duration", duration)
-            tablistData[key]?.set("expend", (duration.toDouble() / maxOnlineSeconds).toFloat())
-        }
-    }
-
 
     @JvmStatic
     fun getPlayerName(info: NetworkPlayerInfo): String {
@@ -124,16 +128,13 @@ object ExtraTab : Module(
             if (FriendManager.isFriend(name))
                 newName = color format newName
         if (onlineTime)
-            try {
-                if (tablistData.containsKey(name)) {
-                    val duration = tablistData[name]?.get("duration") as Long?
-                    newName += " [" + (duration!! / 3600).toString() + ":" + ((duration % 3600) / 60).toString().padStart(2, '0') + "]"
-                }
-            } catch (_: Exception) { }
-//        var online = Duration.ofSeconds(Instant.now().epochSecond - tablistData[name]!!)
-////        name += " [" + online.toHours().toString()
-//        return name
-
+            tablistData[name]?.let {
+                val duration = Instant.now().epochSecond - it
+                newName += " [" +
+                    (duration / 3600).toString() + ":" +
+                    ((duration % 3600) / 60).toString().padStart(2, '0') +
+                    "]"
+            } ?: run { newName += " [0:00]" }
         return newName
     }
 
@@ -141,30 +142,17 @@ object ExtraTab : Module(
     fun subList(list: List<NetworkPlayerInfo>, newList: List<NetworkPlayerInfo>): List<NetworkPlayerInfo> {
         return if (isDisabled) newList else list.subList(0, tabSize.coerceAtMost(list.size))
     }
+
     @JvmStatic
     fun drawExpendRect(left: Int, top: Int, right: Int, bottom: Int, playerName: String) {
-        val width = right - left
-        var barPoint = left
-        try {
-            if (tablistData.containsKey(playerName)) {
-                val expend = tablistData[playerName]?.get("expend") as Float
-                barPoint = (left + (width*expend).toInt())
-            }
-        } catch (ex: Exception) {
-            println(ex)
+        tablistData[playerName]?.let {
+            val barPoint = (left + ((right - left) * ((Instant.now().epochSecond - it).toDouble() / maxOnlineSeconds)).toInt())
+            Gui.drawRect(barPoint, top, right, bottom, 553648127)
+            Gui.drawRect(left, top, barPoint, bottom, rgbToHex(0, 255, 0, 100))
+            GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f)
+            GlStateManager.enableBlend()
         }
-
-        Gui.drawRect(barPoint, top, right, bottom, 553648127)
-        Gui.drawRect(left, top, barPoint, bottom, rgbToHex(0, 255, 0, 100))
-        GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f)
-        GlStateManager.enableAlpha()
-        GlStateManager.enableBlend()
-        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO)
     }
-
-
-
-
 }
 
 
