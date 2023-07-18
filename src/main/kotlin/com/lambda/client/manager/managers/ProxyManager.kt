9fea9508f04e4393fffcdfc9264baed546426b31
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-18 15:04:08 -0700

    pointless extra ops

--- src/main/kotlin/com/lambda/client/manager/managers/ProxyManager.kt
+++ src/main/kotlin/com/lambda/client/manager/managers/ProxyManager.kt
@@ -2,14 +2,12 @@ package com.lambda.client.manager.managers
 
 
 import com.lambda.client.manager.Manager
-import com.lambda.client.util.Wrapper.player
-import com.lambda.client.event.SafeClientEvent
 
 
 object ProxyManager : Manager {
 
     fun isProxy(): Boolean {
         val serverBrand = mc.player.serverBrand ?: "Unknown Server Type"
-        return serverBrand.startsWith("ZenithProxy");
+        return serverBrand == "ZenithProxy";
     }
 }
