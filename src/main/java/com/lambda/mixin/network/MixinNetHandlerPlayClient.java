8ba630866c2c6d0ef20b18565042b9406a4bee04
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-23 20:34:52 -0700

    FakeVanillaClient: block pesky client brand packet

--- src/main/java/com/lambda/mixin/network/MixinNetHandlerPlayClient.java
+++ src/main/java/com/lambda/mixin/network/MixinNetHandlerPlayClient.java
@@ -3,14 +3,18 @@ package com.lambda.mixin.network;
 import com.lambda.client.event.LambdaEventBus;
 import com.lambda.client.event.events.ChunkDataEvent;
 import com.lambda.client.manager.managers.CachedContainerManager;
+import com.lambda.client.module.modules.misc.FakeVanillaClient;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.SPacketChunkData;
 import net.minecraft.network.play.server.SPacketWindowItems;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = NetHandlerPlayClient.class)
@@ -28,5 +32,11 @@ public class MixinNetHandlerPlayClient {
         if (packetIn.getWindowId() != 0) {
             CachedContainerManager.updateContainerInventory(packetIn.getWindowId());
         }
+    }
+
+    @Redirect(method = "handleJoinGame", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkManager;sendPacket(Lnet/minecraft/network/Packet;)V"))
+    public void redirectClientBrandPacket(final NetworkManager instance, final Packet<?> packetIn) {
+        if (FakeVanillaClient.INSTANCE.isEnabled()) return;
+        instance.sendPacket(packetIn);
     }
 }
